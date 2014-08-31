7195366c3dd1dc00295498a5c8ca721618895207
chathura2020 <chathurashan89@gmail.com>
2014-08-31 21:58:21 +0530

    initial commit

---
+++ Assets/GameManager.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+static var playerScore01 :int =0;
+static var playerScore02 :int =0;
+var theSkin: GUISkin;
+ 
+// var theBall : Transform;
+// 
+// function Start(){
+// 	theBall = GameObject.FindGameObjectsWithTag("Ball").transform;
+// 	
+// }
+static function Score (wallName :String) {
+		if(wallName=="RightWall"){ 
+			playerScore01+=1;
+		}else{
+			playerScore02+=1;
+		}
+		
+		Debug.Log("Player score 01 is :"+playerScore01);
+		Debug.Log("Player score 02 is :"+playerScore02);
+}
+
+function OnGUI(){
+	GUI.skin = theSkin;
+	GUI.Label(new Rect(Screen.width/2-150-18,20,100,100),""+playerScore01);
+	GUI.Label(new Rect(Screen.width/2+150-18,20,100,100),""+playerScore02);
+	
+	if(GUI.Button(new Rect(Screen.width/2-121/2,35,121,53),"RESET")){
+		playerScore01=0;
+		playerScore02=0;
+		GameObject.FindGameObjectWithTag("Ball").SendMessage("ResetBall");
+		//theBall.SendMessage("ResetBall");
+	}
+}
---
+++ Assets/PlayerControls.js
@@ -0,0 +1,21 @@
+﻿#pragma strict
+
+var moveDown : KeyCode;
+var moveUp : KeyCode;
+
+var speed : float =10;
+
+function Update () {
+
+	if( Input.GetKey(moveUp) )
+	{
+		rigidbody2D.velocity.y = speed;
+		
+	}else if(Input.GetKey(moveDown))
+	{
+		rigidbody2D.velocity.y = speed * -1;
+	}else
+	{
+		rigidbody2D.velocity.y = 0;
+	}
+}
